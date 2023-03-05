6168c3773bd1b67105ffc018664f4b9dd58360b4
Diego <diego.petrucci@linksmt.it>
2023-03-05 21:43:10 +0100

    creata classe LinkedListNode

--- LinkedList.php
+++ LinkedList.php
@@ -1,4 +1,6 @@
 <?php
+include('LinkedListNode.php');
+
 class LinkedList {
    public $head;
    public $tail; 
@@ -141,35 +143,4 @@ class LinkedList {
     }
 
 }
-   class LinkedListNode{
-    private $next = null;
-    private $prev = null;
-    private $value = null;
-    public function __construct($value)
-    {
-        $this->value = $value;
-    }
-    public function getValue(){
-        return $this->value;
-    }
-    public function setNext(LinkedListNode $node){
-        $this->next = $node;
-    }
-    public function getNext(){
-        return $this->next;
-    }
-    public function setPrev(LinkedListNode $node){
-        $this->prev = $node;
-    }
-    public function getPrev(){
-        return $this->prev;
-    }
-    public function resetPrev(){
-        $this->prev = null;
-    }
-    public function resetNext(){
-        $this->next = null;
-    }
-   }
-
 ?>
