91a457a38ea62a89972dcbcb45a52241c44cfb8b
spyrash <diego.petrucci@outlook.com>
2023-11-29 17:55:52 +0100

    refactor

---
+++ Tests/TestLinkedList.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace LinkedListCustom\Tests;
+
+use LinkedListCustom\LinkedListNode;
+use LinkedListCustom\LinkedList;
+
+function test(){
+$nodo = new LinkedListNode(5);
+$nodo2 = new LinkedListNode(7);
+$nodo3 = new LinkedListNode(8);
+$lista = new LinkedList($nodo);
+$lista->findNode($nodo);
+$lista->pushNode($nodo2);
+$lista->printLinkedList();
+$poppedNode = $lista->popNode();
+$lista->findNode($nodo);
+$lista->printLinkedList();
+$lista->pushNode($poppedNode);
+$lista->printLinkedList();
+$lista->findAtIndex(1);
+$lista->pushNode($nodo3);
+$lista->printLinkedList();
+}
+
+test();
+
+
+
+
+?>
