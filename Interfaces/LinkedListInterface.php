b521e4f9b81e3ecc024d74586b997791e32cc8b2
spyrash <diego.petrucci@outlook.com>
2023-11-29 17:53:21 +0100

    cheanged the return type of "pop" method

--- Interfaces/LinkedListInterface.php
+++ Interfaces/LinkedListInterface.php
@@ -9,5 +9,5 @@ interface LinkedListInterface {
     public function append($data): ?LinkedListNode;
     public function prepend($data): ?LinkedListNode;
     public function delete($data): bool;
-    public function pop(): bool;
+    public function pop(): ?LinkedListNode;
 }
