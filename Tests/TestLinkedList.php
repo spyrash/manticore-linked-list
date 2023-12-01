71dbbea03c9c0e237357f409eabb9c44baa45bf9
spyrash <diego.petrucci@outlook.com>
2023-12-02 00:35:54 +0100

    added TODO in tests

--- Tests/TestLinkedList.php
+++ Tests/TestLinkedList.php
@@ -5,21 +5,22 @@ namespace LinkedListCustom\Tests;
 use LinkedListCustom\LinkedListNode;
 use LinkedListCustom\LinkedList;
 
+//todo: total refactor.
 function test(){
-$nodo = new LinkedListNode(5);
-$nodo2 = new LinkedListNode(7);
-$nodo3 = new LinkedListNode(8);
-$lista = new LinkedList($nodo);
-$lista->findNode($nodo);
-$lista->pushNode($nodo2);
+$node = new LinkedListNode(5);
+$node2 = new LinkedListNode(7);
+$node3 = new LinkedListNode(8);
+$lista = new LinkedList($node);
+$lista->findNode($node);
+$lista->pushNode($node2);
 $lista->printLinkedList();
 $poppedNode = $lista->popNode();
-$lista->findNode($nodo);
+$lista->findNode($node);
 $lista->printLinkedList();
 $lista->pushNode($poppedNode);
 $lista->printLinkedList();
 $lista->findAtIndex(1);
-$lista->pushNode($nodo3);
+$lista->pushNode($node3);
 $lista->printLinkedList();
 }
 
