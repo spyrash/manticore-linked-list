d9866d9c27cf8d90b6a98374c0d4fc4707a8baf9
spyrash <diego.petrucci@outlook.com>
2023-12-05 00:19:26 +0100

    added TODO, refactor of TestLinkedList

--- Tests/TestLinkedList.php
+++ Tests/TestLinkedList.php
@@ -1,32 +1,22 @@
 <?php
 
-namespace LinkedListCustom\Tests;
+namespace ManticoreLinkedList\Tests;
 
-use LinkedListCustom\LinkedListNode;
-use LinkedListCustom\LinkedList;
+use ManticoreLinkedList\src\LinkedListNode;
+use ManticoreLinkedList\src\LinkedList;
 
-//todo: total refactor.
-function test(){
-$node = new LinkedListNode(5);
-$node2 = new LinkedListNode(7);
-$node3 = new LinkedListNode(8);
-$lista = new LinkedList($node);
-$lista->findNode($node);
-$lista->pushNode($node2);
-$lista->printLinkedList();
-$poppedNode = $lista->popNode();
-$lista->findNode($node);
-$lista->printLinkedList();
-$lista->pushNode($poppedNode);
-$lista->printLinkedList();
-$lista->findAtIndex(1);
-$lista->pushNode($node3);
-$lista->printLinkedList();
+class TestLinkedList {
+
+    //TODO: generic test for the linked list, take time of execution and find methods to convert array or "collection" into linkedList
+    public function genericTest():bool
+    {
+        $firstNode = new LinkedListNode();
+        $manticore = new LinkedList($firstNode);
+        $manticore->pop();
+        return true;
+    }
 }
 
-test();
 
 
 
-
-?>
